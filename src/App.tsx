34a8eaf3cd5e8f7ba8881cfe7489a2a9c19b3c0a
Analin Flores <foglzerika@gmail.com>
2023-07-12 23:03:07 -0600

    feat | create appContainer component and adding styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import {AppContainer} from "./assets/components/Layout/appContainer";
 
 function App() {
 
   return (
-    <>
+    <AppContainer>
       <h1>Vite + React</h1>
 
-    </>
+    </AppContainer>
   )
 }
 
---
+++ src/assets/components/Layout/appContainer.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  background-color: var(--primary);
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+`;
