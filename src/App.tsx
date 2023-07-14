67ed839208e70e18d15a59d3f7d2c99eb0fdae2a
Analin Flores <foglzerika@gmail.com>
2023-07-13 20:08:05 -0600

    feat | create columns in app containter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { AppContainer } from "./assets/styles/styles";
 import { AddNewItem } from "./assets/components/Item/AddNewItem";
+import { Column } from './assets/components/Layout/Column';
+import { Card } from './assets/components/DataDisplay/Card';
 
 function App() {
   const handleAdd = () => {
@@ -7,6 +9,19 @@ function App() {
   }
   return (
     <AppContainer>
+      <Column title="To Do" >
+        <Card title="Generate app scaffold" />
+      </Column>
+
+      <Column title="In Progress" >
+        <Card title="Learn Typecript" />
+      </Column>
+
+      <Column title="Done">
+        <Card title="Begin to use static typing" />
+      </Column>
+
+
       <AddNewItem onAdd={() => handleAdd()} toggleButtonText={"add"} />
     </AppContainer>
   )
--- src/assets/components/Layout/Column.tsx
+++ src/assets/components/Layout/Column.tsx
@@ -1,17 +1,27 @@
-import {ReactNode} from "react";
-import {ColumnContainer, ColumnTitle} from "../../styles/styles";
+import { ReactNode } from "react";
+import { ColumnContainer, ColumnTitle } from "../../styles/styles";
+import { AddNewItem } from '../Item/AddNewItem';
 
 type ColumnProps = {
     title: string,
     children?: ReactNode; // best, accepts everything React can render
 }
-export const Column = ({title, children}: ColumnProps) => {
+export const Column = ({ title, children }: ColumnProps) => {
+    const handleAdd = () => {
+        console.log("handle ")
+    }
+
     return (
         <ColumnContainer>
             <ColumnTitle>
                 {title}
             </ColumnTitle>
             {children}
+            <AddNewItem
+                onAdd={() => handleAdd()}
+                toggleButtonText="+ Add another task"
+                dark
+            />
         </ColumnContainer>
     );
 };
